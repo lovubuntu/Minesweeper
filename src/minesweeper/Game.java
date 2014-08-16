26fc8020a470ae2eec2060ee4bdd43c4348c6107
Raj Bharath <rbrajbharath1@gmail.com>
2014-08-16 11:15:35 +0530

    Introduce TotalMineCount inside Game.

--- src/minesweeper/Game.java
+++ src/minesweeper/Game.java
@@ -1,50 +1,50 @@
 package minesweeper;
+
 import java.io.*;
-class Game 
-{
-	public static void main(String arg[]) throws IOException
-	{	
-		
-		int row,col;
-		Cells  box  = new Cells();
+
+class Game {
+	// total mine count should be determined by the Game
+	private final int totalMineCount = 10;
+
+	public static void main(String arg[]) throws IOException {
+
+		int row, col;
+		Cells box = new Cells();
 		Board play = new Board();
-		MineChecker  bomb = new MineChecker();
+		MineChecker bomb = new MineChecker();
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		box.fillCells();
 		box.test();
 		play.fillBoard();
 		play.display();
-		do{			
-			System.out.println("Enter the Row No:(From 0 to 9)")	;
+		do {
+			System.out.println("Enter the Row No:(From 0 to 9)");
 			row = get_User_Val(br);
-			System.out.println("Enter the Col No:(From 0 to 9)")	;
+			System.out.println("Enter the Col No:(From 0 to 9)");
 			col = get_User_Val(br);
 
-			bomb.check_Mine(row,col,box);
-			play.assignValueOfCellAt(row,col,box);			
+			bomb.check_Mine(row, col, box);
+			play.assignValueOfCellAt(row, col, box);
 			play.display();
 			play.calculateUnopenedCellsCount();
-		}while(true);		
+		} while (true);
 	}
-	static int get_User_Val(BufferedReader br) throws IOException
-		{
-			int value = 0;
-			while(true){
-			try{
+
+	static int get_User_Val(BufferedReader br) throws IOException {
+		int value = 0;
+		while (true) {
+			try {
 				value = Integer.parseInt(br.readLine());
-			}
-			catch(NumberFormatException nfe)
-			{
+			} catch (NumberFormatException nfe) {
 				System.out.println("Please Enter a Number");
 				continue;
 			}
-			if(value < 0 || value > 9)
-			{
+			if (value < 0 || value > 9) {
 				System.out.println("Value should be in the range of 0 - 9");
 				continue;
-			}			
-			return value;
 			}
+			return value;
 		}
+	}
 }
---
+++ src/test/AllTests.java
@@ -0,0 +1,12 @@
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BoardTest.class, CellTest.class, ColElementsTest.class,
+		RowElementsTest.class })
+public class AllTests {
+
+}
