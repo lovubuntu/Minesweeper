cedb2561609555e1d5a5948a33d8cf05e0e6be2a
Raj Bharath <rbrajbharath1@gmail.com>
2014-08-16 16:40:34 +0530

    Introduce IOUtil

--- src/minesweeper/Game.java
+++ src/minesweeper/Game.java
@@ -11,15 +11,12 @@ class Game {
 		int row, col;
 
 		Grid grid = new Grid(10, new RandomGridFiller());
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		grid.test();
 		grid.display();
 		do {
-			System.out.println("Enter the Row No:(From 0 to 9)");
-			row = getUserVal(br);
-			System.out.println("Enter the Col No:(From 0 to 9)");
-			col = getUserVal(br);
+			IOUtil.println("Enter the Row No:(From 0 to 9)");
+			row = getUserVal();
+			IOUtil.println("Enter the Col No:(From 0 to 9)");
+			col = getUserVal();
 
 			if (grid.open(row, col)) {
 				grid.display();
@@ -44,24 +41,23 @@ class Game {
 
 	private void decideGameStatus() {
 		if (allMinesIdentified) {
-			System.out.println("Congratulations\n You Won");
+			IOUtil.println("Congratulations\n You Won");
 		} else if (openedMine) {
-			System.out
-					.println("Sorry..You stepped on a MineChecker...\nGame Over");
+			IOUtil.println("Sorry..You stepped on a MineChecker...\nGame Over");
 		}
 	}
 
-	int getUserVal(BufferedReader br) throws IOException {
+	int getUserVal() throws IOException {
 		int value = 0;
 		while (true) {
 			try {
-				value = Integer.parseInt(br.readLine());
+				value = Integer.parseInt(IOUtil.read());
 			} catch (NumberFormatException nfe) {
-				System.out.println("Please Enter a Number");
+				IOUtil.println("Please Enter a Number");
 				continue;
 			}
 			if (value < 0 || value > 9) {
-				System.out.println("Value should be in the range of 0 - 9");
+				IOUtil.println("Value should be in the range of 0 - 9");
 				continue;
 			}
 			return value;
